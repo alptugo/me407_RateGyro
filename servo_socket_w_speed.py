3ff25c564f8cc48e6d897f3a294b9b9b8e22a27b
alptugman <alptugoztaskin@hotmail.com>
2020-04-18 16:18:22 +0300

    python code with socket library
    driving servo with socket library (with simulated speed)

---
+++ servo_socket_w_speed.py
@@ -0,0 +1,26 @@
+import socket
+import time
+import threading as th
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("192.168.4.1", 80))
+do_i_go=1
+delay=100
+def loop():
+    global delay, do_i_go
+    while 1:
+        if do_i_go:
+            for step in range(40,120):
+                s.send((str(step)+"s").encode("UTF-8"))
+                time.sleep(delay/1000)
+
+t=th.Thread(target=loop)
+t.start()
+while 1:
+    input_=input("gimme\n")
+    if input_:
+        delay=int(input_)
+        if not do_i_go:
+            do_i_go=1
+    else:
+        do_i_go=0
