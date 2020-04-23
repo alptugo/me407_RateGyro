05a271e5957cbe6f25d689808900fdd1249129f4
alptugman <alptugoztaskin@hotmail.com>
2020-04-23 11:17:00 +0300

    Add files via upload

--- arda/arda_servo_socket_w_speed.py
+++ arda/arda_servo_socket_w_speed.py
@@ -1,21 +1,11 @@
 import socket
-import time
-import threading as th
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("192.168.4.1", 80))
 do_i_go=1
 delay=100
-def loop():
-    global delay, do_i_go
-    while 1:
-        if do_i_go:
-            for step in range(40,120):
-                s.send((str(step)+"s").encode("UTF-8"))
-                time.sleep(delay/1000)
 
-#t=th.Thread(target=loop)
-#t.start()
 while 1:
     input_=input("gimme\n")
     if input_:
